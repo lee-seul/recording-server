217d2dd0b14f335eca6e49ccd5d40a0bd64d149b
seul lee <blacksangi14@naver.com>
2018-05-05 21:36:15 +0900

    add create table method

--- recording/db_handler.py
+++ recording/db_handler.py
@@ -10,11 +10,31 @@ class DynamoDB(object):
         self.conn = boto3.resource('dynamodb', region_name='ap-northeast-2')
 
     def create_table(
-            self, table_name, hash_name, 
+            self, table_name, hash_dict, range_dict, attrs,
             read_throughput=5, write_throughput=5):
         
         dynamodb = self.conn
         
-        #table = dynamodb.create_table(
-        #        TableName=table_name,
-        #        Key
+        attrs += [hash_dict, range_dict]
+
+        table = dynamodb.create_table(
+                TableName=table_name,
+                AttrebuteDefinitions=attr,
+                KeySchema=[
+                    {
+                        'AttributeName': hash_dict['AttributeName'],
+                        'KeyType': 'HASH'
+                    },
+                    {
+                        'AttributeName': range_dict['AttributeName'],
+                        'KeyType': 'RANGE'
+                    }
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': read_throughput,
+                    'WriteCapacityUnits': write_throughput
+                } 
+            )
+        return table
+
+
